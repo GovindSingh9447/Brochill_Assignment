dadc53b3a4bafe07a3a7499b4775222cb999aee7
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-05-12 13:19:12 +0530

    Connecting with Api

--- app/src/main/java/com/ranawat/brochill_assignment/Interface/Api.java
+++ app/src/main/java/com/ranawat/brochill_assignment/Interface/Api.java
@@ -1,10 +1,21 @@
 package com.ranawat.brochill_assignment.Interface;
 
+import com.ranawat.brochill_assignment.ModelResponse.RegisterResponse;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface Api {
-/*
-    @POST()
-    Call<>*/
+
+    @FormUrlEncoded
+    @POST("register")
+    Call<RegisterResponse> register(
+            @Field("first_name") String first_name,
+            @Field("last_name") String last_name,
+            @Field("email") String email,
+            @Field("password") String password
+
+    );
 }
